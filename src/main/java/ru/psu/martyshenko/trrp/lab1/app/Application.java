2bb2ea27f39d55fc893c259ebe769a53d1cbaa05
LimonPudding <limonpudding@gmail.com>
2020-09-30 22:59:56 +0500

    Добавлена возможность удаления учётных записей.

--- src/main/java/ru/psu/martyshenko/trrp/lab1/app/Application.java
+++ src/main/java/ru/psu/martyshenko/trrp/lab1/app/Application.java
@@ -9,7 +9,10 @@ import com.google.api.services.calendar.model.Event;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.logging.LogManager;
 
 import static ru.psu.martyshenko.trrp.lab1.app.GlobalSettings.TOKENS_DIRECTORY_PATH;
 
@@ -21,53 +24,57 @@ public class Application {
     private static Credential credential = null;
 
     public static void main(String[] args) {
+        LogManager.getLogManager().reset(); // Отключение логгера библиотеки гугл
+        org.mortbay.log.Log.setLog(null); // Отключение еще одного логгера библиотеки гугл
         mainMenu();
     }
 
     private static void mainMenu() {
-        System.out.println("Выберите опцию:");
-        System.out.println("1. Войти в учетную запись");
-        System.out.println("2. Удалить учетную запись");
-        System.out.println("0. Выход из приложения");
-        int option = Integer.parseInt(in.nextLine());
-        while (option != 0) {
+        int option;
+        do {
+            System.out.println("Выберите опцию:");
+            System.out.println("1. Войти в учетную запись");
+            System.out.println("2. Удалить учетную запись");
+            System.out.println("0. Выход из приложения");
+            option = Integer.parseInt(in.nextLine());
             switch (option) {
+                case 0:
+                    break;
                 case 1:
-                    selectProfile();
-                    selectAction();
-                    option = 0;
+                    String user = selectProfile();
+                    if (user != null) {
+                        selectAction(user);
+                    }
                     break;
                 case 2:
-//                    deleteProfile();
-                    option = 0;
+                    deleteProfile();
                     break;
                 default:
-                    System.out.println("Неверный ввод! Пожалуйста, ведите 0 или 1.");
+                    System.out.println("Неверный ввод! Пожалуйста, ведите 0, 1 или 2.");
                     option = Integer.parseInt(in.nextLine());
                     break;
             }
-        }
+        } while (option != 0);
     }
 
-    private static void selectProfile() {
-        ArrayList<String> storedCredential;
-        try {
-            FileDataStoreFactory storedCredentialData = new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH));
-            storedCredential = new ArrayList<>(storedCredentialData.getDataStore("StoredCredential").keySet());
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
+    private static String selectProfile() {
+        ArrayList<String> storedCredential = loadTokens();
+        String userName = null;
+        if (storedCredential == null) {
+            return null;
         }
 
         System.out.println("Выберите действие:");
         System.out.println("1. Вход в учётную запись Google");
-        System.out.println("Использовать сохранённую учётную запись Google:");
+        if (!storedCredential.isEmpty()) {
+            System.out.println("Использовать сохранённую учётную запись Google:");
+        }
         int counter = 1;
         for (String key : storedCredential) {
             counter += 1;
             System.out.println(counter + ". " + key);
         }
-        System.out.println("0. Выход из приложения");
+        System.out.println("0. Назад, в меню авторизации");
         try {
             HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
         } catch (GeneralSecurityException | IOException e) {
@@ -78,7 +85,7 @@ public class Application {
         while (option != 0) {
             if (option == 1) {
                 System.out.println("Придумайте имя для учётной записи:");
-                String userName = in.nextLine();
+                userName = in.nextLine();
                 try {
                     credential = authService.auth(HTTP_TRANSPORT, userName);
                 } catch (IOException e) {
@@ -88,7 +95,8 @@ public class Application {
             } else {
                 if (option > 1 && option <= maxOption) {
                     try {
-                        credential = authService.auth(HTTP_TRANSPORT, storedCredential.get(option - 2));
+                        userName = storedCredential.get(option - 2);
+                        credential = authService.auth(HTTP_TRANSPORT, userName);
                     } catch (IOException e) {
                         System.out.println("Произошла ошибка при попытке авторизации.");
                     }
@@ -99,16 +107,73 @@ public class Application {
                 }
             }
         }
+        return userName;
     }
 
-    private static void selectAction() {
-        System.out.println("Успешная авторизация!");
-        actionMenuForLoggedIn();
-        EventService eventService = new EventService(HTTP_TRANSPORT, credential);
-        in.reset();
+    private static void deleteProfile() {
+        ArrayList<String> storedCredential = loadTokens();
+        if (storedCredential == null) {
+            return;
+        }
+        if (storedCredential.isEmpty()) {
+            System.out.println("Не найдено ни одной сохранённой учётной записи!");
+            return;
+        }
+        System.out.println("Какую учётную запись требуется удалить?");
+        int counter = 0;
+        Map<Integer, String> credentials = new HashMap<>();
+        for (String key : storedCredential) {
+            counter += 1;
+            credentials.put(counter, key);
+            System.out.println(counter + ". " + key);
+        }
+        System.out.println("0. Выход из приложения");
+        int maxOption = storedCredential.size();
         int option = Integer.parseInt(in.nextLine());
+
         while (option != 0) {
+            if (option > 0 && option <= maxOption) {
+                deleteToken(credentials.get(option));
+                option = 0;
+            } else {
+                System.out.println("Неверный ввод! Пожалуйста, ведите число от 0 до " + maxOption);
+                option = Integer.parseInt(in.nextLine());
+            }
+        }
+    }
+
+    private static void deleteToken(String user) {
+        try {
+            FileDataStoreFactory fdsf = new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH));
+            fdsf.getDataStore("StoredCredential").delete(user);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static ArrayList<String> loadTokens() {
+        ArrayList<String> storedCredential;
+        try {
+            FileDataStoreFactory storedCredentialData = new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH));
+            storedCredential = new ArrayList<>(storedCredentialData.getDataStore("StoredCredential").keySet());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return storedCredential;
+    }
+
+    private static void selectAction(String user) {
+        System.out.println("Успешная авторизация, " + user + "!");
+        in.reset();
+        EventService eventService = new EventService(HTTP_TRANSPORT, credential);
+        int option;
+        do {
+            actionMenuForLoggedIn();
+            option = Integer.parseInt(in.nextLine());
             switch (option) {
+                case 0:
+                    break;
                 case 1:
                     try {
                         eventService.showEvents();
@@ -139,9 +204,7 @@ public class Application {
                     System.out.println("Неверный ввод! Пожалуйста, ведите число от 0 до 4.");
                     break;
             }
-            actionMenuForLoggedIn();
-            option = Integer.parseInt(in.nextLine());
-        }
+        } while (option != 0);
     }
 
     private static void actionMenuForLoggedIn() {
@@ -150,7 +213,7 @@ public class Application {
         System.out.println("2. Создать новое событие");
         System.out.println("3. Редактировать событие");
         System.out.println("4. Удалить событие");
-        System.out.println("0. Выход из приложения");
+        System.out.println("0. Выход из учётной записи");
     }
 
     private static void prepareEventInfo(EventDto eventDto) {
